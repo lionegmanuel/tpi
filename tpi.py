ee9939793683de1552f468108a370fd4b54df05e
MagnuZvn <lionegmanuel@gmail.com>
2023-11-07 21:06:09 -0300

    funcionalidades unidades de medida

--- tpi.py
+++ tpi.py
@@ -1,7 +1,10 @@
 import os, datetime
 
-def floatValidate(number):
-  chars = '0123456789.'
+def numberValidate(number, type):
+  match type:
+    case 'float': chars = '0123456789.'
+    case 'int': chars = '0123456789'
+  
   for char in number:
     if char not in chars: return False
   return True
@@ -12,33 +15,51 @@ def yesOrNoValidate(str):
    return True
 def strValidate(str):
   str = str.upper()
-  chars = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+  chars = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ '
   for char in str:
     if char.upper() not in chars: return False
   return True
 #1)- Interfaz
 def dataGenerator():
     data = []
+    waste = 0 #margen de desperdicio por defecto (en caso de que no se ingrese otro valor posteriormente)
     clientName = input('Nombre del cliente:\n\t=> ')
     while(not(strValidate(clientName))): clientName = input('¡ERROR!\nIngrese un nombre válido.\n\t=> ')
+    #unidad con la que se trabajará... (metros, pies, centimetros, kilometros, yardas...)
+    while True:
+      unitOfMeasure = input('Unidad de medida de registro de los datos de la superficie:\n1-Metros\n2-Pies\n3-Kilómetros\n4-Centímetros\n5-Yardas\n\t=> ')
+      if not unitOfMeasure.isnumeric():
+        print('\n¡ERROR!\nIngrese una opción válida.\n')
+      else:
+         if (not(numberValidate(unitOfMeasure, 'int'))): unitOfMeasure = print('\n¡ERROR!\nIngrese una opción válida.\n')
+         else:
+          if 1<=int(unitOfMeasure) and int(unitOfMeasure)<=5: break
+          else: print('\n¡ERROR!\nIngrese una opción válida.\n')  
+    #confirmación de unidad de medida
+    match unitOfMeasure:
+      case int(1): print('\n--Unidad de medida: METROS CÚBICOS [m³]--\n')
+      case int(2): print('\n--Unidad de medida: PIES CÚBICOS [ft³]--\n')
+      case int(3): print('\n--Unidad de medida: KILÓMETROS CÚBICOS [km³]--\n')
+      case int(4): print('\n--Unidad de medida: CENTÍMETROS CÚBICOS [cm³]--\n')
+      case int(5): print('\n--Unidad de medida: YARDAS CÚBICAS [yd³]--\n') 
     length = input('Longitud de la superficie:\n\t=> ')
-    while(not(floatValidate(length))): length = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
+    while(not(numberValidate(length, 'float') and float(length) > 0 )): length = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
     width = input('Ancho de la superficie:\n\t=> ')
-    while(not(floatValidate(width))): width = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
+    while(not(numberValidate(width, 'float') and float(width) > 0)): width = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
     height = input('Altura de la superficie:\n\t=> ')
-    while(not(floatValidate(height))): height = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
+    while(not(numberValidate(height, 'float') and float(height) > 0 )): height = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
     wasteMargin = input('¿Se utilizará un margen de desperdicio para el material?\n!(S / N)\n\t=> ')
     while(not(yesOrNoValidate(wasteMargin))): wasteMargin = input('¡Respuesta inválida!\n\t=> ')
     if (wasteMargin.upper() == 'S'): 
        waste = input('Margen de desperdicio de hormigón:\n\t=> ')
-       while(not(floatValidate(waste))): waste=input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
+       while(not(numberValidate(waste, 'float'))): waste=input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
     density = input('Densidad deseada del material:\n\t=> ')
-    while (not(floatValidate(density))): density = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
+    while (not(numberValidate(density, 'float'))): density = input('¡ERROR!\nIngrese una cantidad válida.\n\t=> ')
     data.append(float(length))
     data.append(float(width))
     data.append(float(height))
     data.append(float(density))
-    data.append(float(waste)/100)#calculo del valor % para utilizarlo de forma directa => 10% = 0.1
+    data.append(float(waste)/100 if float(waste) > 0 else 0)#calculo del valor % para utilizarlo de forma directa => 10% = 0.1
     data.append(clientName)
     return data
 
@@ -54,14 +75,17 @@ def calculate(clientData):
 def orderData (name, amountOfMaterial):
     currentDate = datetime.datetime.now()
     dateFormatted = currentDate.strftime('%Y-%m-%d %H:%M:%S')
-    return (f'---HISTORIAL---\n\t✔️ Fecha de pedido: {dateFormatted}\n✔️ Cliente: {name}\n✔️ Cantidad de material solicitado: {amountOfMaterial} metros cúbicos (m³)')
+    return (f'\t✔️ Fecha de pedido: {dateFormatted}\n\t✔️ Cliente: {name}\n\t✔️ Cantidad de material solicitado: {amountOfMaterial} metros cúbicos (m³)')
 
 def ticketGenerator (data, calc):
-    data = (f'---INFORMACIÓN DE PEDIDO---\nCliente: {data[5]}\n*Superficie solicitada:\n\t1-Longitud: {data[0]}\n\t2-Ancho: {data[1]}\n\t3-Altura: {data[2]}\n*Material\n\t1-Cantidad solicitada: {calc[1]} metros cúbicos (m³)\n\t2-Peso: {calc[3]}kg\n*Datos sobre el material:\n\t1-Densidad utilizada: {data[3]}\n\t2-Margen (%) de desperdicio: {(data[4]*100)}')
+    wasteResponse = (f'{(data[4]*100)}%' if data[4] >0 else 'N/A') #uso de operaciones ternarias para definir el valor final
+    densityResponse = (f'{data[3]}kg/m³' if data[3] > 0 else 'N/A')
+    weightResponse = (f'{calc[3]}kg' if calc[3] > 0 else '"Sin densidad definida"')
+    ticketData = (f'\n---INFORMACIÓN DE PEDIDO---\n\nCliente: {data[5]}\n*Superficie solicitada:\n\t1-Longitud: {data[0]}\n\t2-Ancho: {data[1]}\n\t3-Altura: {data[2]}\n*Material\n\t1-Cantidad solicitada: {calc[1]}m³\n\t2-Peso: {weightResponse}\n*Datos sobre el material:\n\t1-Densidad utilizada: {densityResponse}\n\t2-Margen de desperdicio: {wasteResponse}')
     try:
       with open('ticket.txt', 'w') as file:
-        file.write(data)
-      print(data)
+        file.write(ticketData)
+      print(ticketData)
     except: print('Ocurrió un error al generar el ticket.')        
 
 #3)- Gestión de Archivos / Registros / Pedidos
@@ -76,11 +100,10 @@ def addOrder(order):
 def main():
   #historial previo
   try:
-     print('--HISTORIAL DE VENTAS--')
-     with open('orderHistory.txt', 'r') as file:
+     with open('orderHistory.txt', 'r', encoding='utf-8') as file:
         data = file.readlines()
         for row in data:
-           print(data[row])
+           print(row)
   except FileNotFoundError: print('\nNo hay ventas registradas\n') 
   
   area = dataGenerator()
